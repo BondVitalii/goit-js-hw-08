6a59b51dedc698e0e1587eb068986204e2f14429
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2023-06-02 16:13:35 +0300

    editing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -15,19 +15,19 @@ const updateTimePlay = throttle(function (data) {
 
 player.on('timeupdate', updateTimePlay);
 
-player
-  .setCurrentTime(lastPlayTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+player.setCurrentTime(lastPlayTime || 0);
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+// .then(function (seconds) {
+//   // seconds = the actual time that the player seeked to
+// })
+// .catch(function (error) {
+//   switch (error.name) {
+//     case 'RangeError':
+//       // the time was less than 0 or greater than the video’s duration
+//       break;
+
+//     default:
+//       // some other error occurred
+//       break;
+//   }
+// });
